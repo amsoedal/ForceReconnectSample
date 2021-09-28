b20d408ab11688566cf6ba2f206349a1a0735399
Michelle Soedal <ansoedal@microsoft.com>
2021-09-28 15:28:42 +0100

    Incorporate better style from quickstart and use SemaphoreSlim

--- ForceReconnect/Program.cs
+++ ForceReconnect/Program.cs
@@ -11,10 +11,10 @@ namespace ForceReconnect
     internal static class Redis
     {
         private static long lastReconnectTicks = DateTimeOffset.MinValue.UtcTicks;
-        private static DateTimeOffset firstError = DateTimeOffset.MinValue;
-        private static DateTimeOffset previousError = DateTimeOffset.MinValue;
+        private static DateTimeOffset firstErrorTime = DateTimeOffset.MinValue;
+        private static DateTimeOffset previousErrorTime = DateTimeOffset.MinValue;
 
-        private static object reconnectLock = new object();
+        private static SemaphoreSlim semaphore = new SemaphoreSlim(initialCount: 1, maxCount: 1);
 
         // In general, let StackExchange.Redis handle most reconnects,
         // so limit the frequency of how often this will actually reconnect.
@@ -25,9 +25,9 @@ namespace ForceReconnect
         public static TimeSpan ReconnectErrorThreshold = TimeSpan.FromSeconds(30);
 
         private static string connectionString = "TODO: CALL InitializeConnectionString() method with connection string";
-        private static Lazy<ConnectionMultiplexer> multiplexer = CreateMultiplexer();
+        private static Lazy<ConnectionMultiplexer> lazyConnection = CreateConnection();
 
-        public static ConnectionMultiplexer Connection { get { return multiplexer.Value; } }
+        public static ConnectionMultiplexer Connection { get { return lazyConnection.Value; } }
 
         public static void InitializeConnectionString(string cnxString)
         {
@@ -40,68 +40,77 @@ namespace ForceReconnect
         /// <summary>
         /// Force a new ConnectionMultiplexer to be created.
         /// NOTES:
-        ///     1. Users of the ConnectionMultiplexer MUST handle ObjectDisposedExceptions, which can now happen as a result of calling ForceReconnect()
-        ///     2. Don't call ForceReconnect for Timeouts, just for RedisConnectionExceptions or SocketExceptions
-        ///     3. Call this method every time you see a connection exception, the code will wait to reconnect:
-        ///         a. for at least the "ReconnectErrorThreshold" time of repeated errors before actually reconnecting
+        ///     1. Users of the ConnectionMultiplexer MUST handle ObjectDisposedExceptions, which can now happen as a result of calling ForceReconnect().
+        ///     2. Don't call ForceReconnect for Timeouts, just for RedisConnectionExceptions or SocketExceptions.
+        ///     3. Call this method every time you see a connection exception. The code will:
+        ///         a. wait to reconnect for at least the "ReconnectErrorThreshold" time of repeated errors before actually reconnecting
         ///         b. not reconnect more frequently than configured in "ReconnectMinFrequency"
         /// </summary>
         public static void ForceReconnect()
         {
             var utcNow = DateTimeOffset.UtcNow;
-            var previousTicks = Interlocked.Read(ref lastReconnectTicks);
-            var previousReconnect = new DateTimeOffset(previousTicks, TimeSpan.Zero);
-            var elapsedSinceLastReconnect = utcNow - previousReconnect;
+            long previousTicks = Interlocked.Read(ref lastReconnectTicks);
+            var previousReconnectTime = new DateTimeOffset(previousTicks, TimeSpan.Zero);
+            TimeSpan elapsedSinceLastReconnect = utcNow - previousReconnectTime;
 
-            // If mulitple threads call ForceReconnect at the same time, we only want to honor one of them.
-            if (elapsedSinceLastReconnect > ReconnectMinFrequency)
+            // If multiple threads call ForceReconnect at the same time, we only want to honor one of them.
+            if (elapsedSinceLastReconnect < ReconnectMinFrequency)
             {
-                lock (reconnectLock)
+                return;
+            }
+
+            semaphore.Wait();
+            try
+            {
+                utcNow = DateTimeOffset.UtcNow;
+                elapsedSinceLastReconnect = utcNow - previousReconnectTime;
+
+                if (firstErrorTime == DateTimeOffset.MinValue)
                 {
-                    utcNow = DateTimeOffset.UtcNow;
-                    elapsedSinceLastReconnect = utcNow - previousReconnect;
-
-                    if (firstError == DateTimeOffset.MinValue)
-                    {
-                        // We haven't seen an error since last reconnect, so set initial values.
-                        firstError = utcNow;
-                        previousError = utcNow;
-                        return;
-                    }
-
-                    if (elapsedSinceLastReconnect < ReconnectMinFrequency)
-                        return; // Some other thread made it through the check and the lock, so nothing to do.
-
-                    var elapsedSinceFirstError = utcNow - firstError;
-                    var elapsedSinceMostRecentError = utcNow - previousError;
-
-                    var shouldReconnect =
-                        elapsedSinceFirstError >= ReconnectErrorThreshold   // make sure we gave the multiplexer enough time to reconnect on its own if it can
-                        && elapsedSinceMostRecentError <= ReconnectErrorThreshold; //make sure we aren't working on stale data (e.g. if there was a gap in errors, don't reconnect yet).
-
-                    // Update the previousError timestamp to be now (e.g. this reconnect request)
-                    previousError = utcNow;
-
-                    if (shouldReconnect)
-                    {
-                        firstError = DateTimeOffset.MinValue;
-                        previousError = DateTimeOffset.MinValue;
-
-                        var oldMultiplexer = multiplexer;
-                        CloseMultiplexer(oldMultiplexer);
-                        multiplexer = CreateMultiplexer();
-                        Interlocked.Exchange(ref lastReconnectTicks, utcNow.UtcTicks);
-                    }
+                    // We haven't seen an error since last reconnect, so set initial values.
+                    firstErrorTime = utcNow;
+                    previousErrorTime = utcNow;
+                    return;
                 }
+
+                if (elapsedSinceLastReconnect < ReconnectMinFrequency)
+                    return; // Some other thread made it through the check and the lock, so nothing to do.
+
+                TimeSpan elapsedSinceFirstError = utcNow - firstErrorTime;
+                TimeSpan elapsedSinceMostRecentError = utcNow - previousErrorTime;
+
+                bool shouldReconnect =
+                    elapsedSinceFirstError >= ReconnectErrorThreshold // Make sure we gave the multiplexer enough time to reconnect on its own if it could.
+                    && elapsedSinceMostRecentError <= ReconnectErrorThreshold; // Make sure we aren't working on stale data (e.g. if there was a gap in errors, don't reconnect yet).
+
+                // Update the previousErrorTime timestamp to be now (e.g. this reconnect request).
+                previousErrorTime = utcNow;
+
+                if (!shouldReconnect)
+                {
+                    return;
+                }
+
+                firstErrorTime = DateTimeOffset.MinValue;
+                previousErrorTime = DateTimeOffset.MinValue;
+
+                Lazy<ConnectionMultiplexer> oldConnection = lazyConnection;
+                CloseConnection(oldConnection);
+                lazyConnection = CreateConnection();
+                Interlocked.Exchange(ref lastReconnectTicks, utcNow.UtcTicks);
+            }
+            finally
+            {
+                semaphore.Release();
             }
         }
 
-        private static Lazy<ConnectionMultiplexer> CreateMultiplexer()
+        private static Lazy<ConnectionMultiplexer> CreateConnection()
         {
             return new Lazy<ConnectionMultiplexer>(() => ConnectionMultiplexer.Connect(connectionString));
         }
 
-        private static void CloseMultiplexer(Lazy<ConnectionMultiplexer> oldMultiplexer)
+        private static void CloseConnection(Lazy<ConnectionMultiplexer> oldMultiplexer)
         {
             if (oldMultiplexer != null)
             {

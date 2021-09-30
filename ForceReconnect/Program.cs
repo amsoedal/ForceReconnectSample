5b2ccbfb82cd66e6474121117ecd4b1fecc50598
Michelle Soedal <ansoedal@microsoft.com>
2021-09-30 13:08:14 +0100

    add async versions of the methods

--- ForceReconnect/Program.cs
+++ ForceReconnect/Program.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Threading;
+using System.Threading.Tasks;
 using StackExchange.Redis;
 
 namespace ForceReconnect
@@ -19,7 +20,7 @@ namespace ForceReconnect
 
         // In general, let StackExchange.Redis handle most reconnects,
         // so limit the frequency of how often this will actually reconnect.
-        public static TimeSpan ReconnectMinFrequency = TimeSpan.FromSeconds(60);
+        public static TimeSpan ReconnectMinInterval = TimeSpan.FromSeconds(60);
 
         // if errors continue for longer than the below threshold, then the
         // multiplexer seems to not be reconnecting, so re-create the multiplexer
@@ -57,7 +58,7 @@ namespace ForceReconnect
             TimeSpan elapsedSinceLastReconnect = utcNow - previousReconnectTime;
 
             // If multiple threads call ForceReconnect at the same time, we only want to honor one of them.
-            if (elapsedSinceLastReconnect < ReconnectMinFrequency)
+            if (elapsedSinceLastReconnect < ReconnectMinInterval)
             {
                 return;
             }
@@ -84,7 +85,7 @@ namespace ForceReconnect
                     return;
                 }
 
-                if (elapsedSinceLastReconnect < ReconnectMinFrequency)
+                if (elapsedSinceLastReconnect < ReconnectMinInterval)
                     return; // Some other thread made it through the check and the lock, so nothing to do.
 
                 TimeSpan elapsedSinceFirstError = utcNow - firstErrorTime;
@@ -117,6 +118,83 @@ namespace ForceReconnect
             }
         }
 
+        /// <summary>
+        /// Force a new ConnectionMultiplexer to be created.
+        /// NOTES:
+        ///     1. Users of the ConnectionMultiplexer MUST handle ObjectDisposedExceptions, which can now happen as a result of calling ForceReconnectAsync().
+        ///     2. Don't call ForceReconnectAsync for Timeouts, just for RedisConnectionExceptions or SocketExceptions.
+        ///     3. Call this method every time you see a connection exception. The code will:
+        ///         a. wait to reconnect for at least the "ReconnectErrorThreshold" time of repeated errors before actually reconnecting
+        ///         b. not reconnect more frequently than configured in "ReconnectMinFrequency"
+        /// </summary>
+        public static async Task ForceReconnectAsync()
+        {
+            var utcNow = DateTimeOffset.UtcNow;
+            long previousTicks = Interlocked.Read(ref lastReconnectTicks);
+            var previousReconnectTime = new DateTimeOffset(previousTicks, TimeSpan.Zero);
+            TimeSpan elapsedSinceLastReconnect = utcNow - previousReconnectTime;
+
+            // If multiple threads call ForceReconnect at the same time, we only want to honor one of them.
+            if (elapsedSinceLastReconnect < ReconnectMinInterval)
+            {
+                return;
+            }
+
+            try
+            {
+                await reconnectSemaphore.WaitAsync(RestartConnectionTimeout);
+            }
+            catch
+            {
+                return;
+            }
+
+            try
+            {
+                utcNow = DateTimeOffset.UtcNow;
+                elapsedSinceLastReconnect = utcNow - previousReconnectTime;
+
+                if (firstErrorTime == DateTimeOffset.MinValue)
+                {
+                    // We haven't seen an error since last reconnect, so set initial values.
+                    firstErrorTime = utcNow;
+                    previousErrorTime = utcNow;
+                    return;
+                }
+
+                if (elapsedSinceLastReconnect < ReconnectMinInterval)
+                    return; // Some other thread made it through the check and the lock, so nothing to do.
+
+                TimeSpan elapsedSinceFirstError = utcNow - firstErrorTime;
+                TimeSpan elapsedSinceMostRecentError = utcNow - previousErrorTime;
+
+                bool shouldReconnect =
+                    elapsedSinceFirstError >= ReconnectErrorThreshold // Make sure we gave the multiplexer enough time to reconnect on its own if it could.
+                    && elapsedSinceMostRecentError <= ReconnectErrorThreshold; // Make sure we aren't working on stale data (e.g. if there was a gap in errors, don't reconnect yet).
+
+                // Update the previousErrorTime timestamp to be now (e.g. this reconnect request).
+                previousErrorTime = utcNow;
+
+                if (!shouldReconnect)
+                {
+                    return;
+                }
+
+                firstErrorTime = DateTimeOffset.MinValue;
+                previousErrorTime = DateTimeOffset.MinValue;
+
+                ConnectionMultiplexer oldConnection = connection;
+                await CloseConnectionAsync(oldConnection);
+                connection = null;
+                await CreateConnectionAsync();
+                Interlocked.Exchange(ref lastReconnectTicks, utcNow.UtcTicks);
+            }
+            finally
+            {
+                reconnectSemaphore.Release();
+            }
+        }
+
         private static ConnectionMultiplexer CreateConnection()
         {
             if (connection != null)
@@ -131,7 +209,7 @@ namespace ForceReconnect
             }
             catch
             {
-                // We failed to enter the semaphore. Connection will either be null, or have a value that was created by another thread.
+                // We failed to enter the semaphore in the given amount of time. Connection will either be null, or have a value that was created by another thread.
                 return connection;
             }
 
@@ -154,6 +232,43 @@ namespace ForceReconnect
             }
         }
 
+        private static async Task<ConnectionMultiplexer> CreateConnectionAsync()
+        {
+            if (connection != null)
+            {
+                // If we already have a good connection, let's re-use it
+                return connection;
+            }
+
+            try
+            {
+                await initSemaphore.WaitAsync(RestartConnectionTimeout);
+            }
+            catch
+            {
+                // We failed to enter the semaphore in the given amount of time. Connection will either be null, or have a value that was created by another thread.
+                return connection;
+            }
+
+            // We entered the semaphore successfully.
+            try
+            {
+                if (connection != null)
+                {
+                    // Another thread must have finished creating a new connection while we were waiting to enter the semaphore. Let's use it
+                    return connection;
+                }
+
+                // Otherwise, we really need to create a new connection.
+                connection = await ConnectionMultiplexer.ConnectAsync(connectionString);
+                return connection;
+            }
+            finally
+            {
+                initSemaphore.Release();
+            }
+        }
+
         private static void CloseConnection(ConnectionMultiplexer oldConnection)
         {
             if (oldConnection != null)
@@ -161,6 +276,21 @@ namespace ForceReconnect
                 try
                 {
                     oldConnection.Close();
+                }
+                catch (Exception)
+                {
+                    // Example error condition: if accessing oldConnection causes a connection attempt and that fails.
+                }
+            }
+        }
+
+        private static async Task CloseConnectionAsync(ConnectionMultiplexer oldConnection)
+        {
+            if (oldConnection != null)
+            {
+                try
+                {
+                    await oldConnection.CloseAsync();
                 }
                 catch (Exception)
                 {
